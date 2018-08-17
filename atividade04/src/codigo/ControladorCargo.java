91c91499cb54f4d605294c98c43355c8902f262c
JaTemJeff <jff_94@hotmail.com>
2018-08-17 15:41:16 -0300

    Refatoração

--- atividade04/src/codigo/ControladorCargo.java
+++ atividade04/src/codigo/ControladorCargo.java
@@ -1,57 +1,45 @@
 package codigo;
 
 public class ControladorCargo {
-    int salarioAtualizado;
 
     public float calcular(String cargo, float salario) throws Exception {
-        if (salario == 0){ 
-            throw new Exception("Informe um salário maior que 0!"); 
-        }else if (cargo == ""){
-            throw new Exception("Informe um cargo!"); 
-        }else if (cargo != "DESENVOLVEDOR" && cargo != "DBA" && cargo != "TESTADOR" && cargo != "GERENTE"){
+        if (salario == 0) {
+            throw new Exception("Informe um salário maior que 0!");
+        } else if (cargo == "") {
+            throw new Exception("Informe um cargo!");
+        } else if (cargo != "DESENVOLVEDOR" && cargo != "DBA" && cargo != "TESTADOR" && cargo != "GERENTE") {
             throw new Exception("Informe um cargo válido!\nDESENVOLVEDOR\nDBA\nTESTADOR\nGERENTE");
-        }
-        
-        else{
+        } else {
             if (cargo == "DESENVOLVEDOR") {
                 if (salario >= 3000) {
-                    //-20%
                     return (salario - (salario * 0.2f));
                 } else {
-                    //-10%
                     return (salario - (salario * 0.1f));
                 }
             }
             if (cargo == "DBA") {
                 if (salario >= 2000) {
-                    //-25%
-                    return (salario -(salario*0.25f));
+                    return (salario - (salario * 0.25f));
                 } else {
-                    //-15%
                     return (salario - (salario * 0.15f));
                 }
             }
-        
+
             if (cargo == "TESTADOR") {
                 if (salario >= 2000) {
-                    //-25%
                     return (salario - (salario * 0.25f));
                 } else {
-                    //-15%
                     return (salario - (salario * 0.15f));
                 }
             }
             if (cargo == "GERENTE") {
                 if (salario >= 5000) {
-                    //-30%
                     return (salario - (salario * 0.3f));
                 } else {
-                    //-20%
                     return (salario - (salario * 02f));
                 }
             }
         }
         return salario;
     }
-
 }
